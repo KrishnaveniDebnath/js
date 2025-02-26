07f1404e124ed02d3c67536dbbe900f186a17c6f
KrishnaveniDebnath <138001013+KrishnaveniDebnath@users.noreply.github.com>
2025-02-26 11:26:51 +0000

    Pending changes exported from your codespace

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,13 @@
+// alert(3+3)
+// this only work in browser and here we r working on terminal
+
+// string=""
+// numbers
+// bigint
+// boolean=>true/false
+// null=> standard value/it is a type of objects
+//undefined=>when we only declare a variable it shows this
+// symbols=>uniqe
+
+let age=30
+console.log(typeof age);
