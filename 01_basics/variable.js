6e6809acff36e19a84326848b99043d5cbec312d
KrishnaveniDebnath <138001013+KrishnaveniDebnath@users.noreply.github.com>
2025-02-26 11:35:57 +0000

    commit_dataTypeAndVariables

---
+++ 01_basics/variable.js
@@ -0,0 +1,15 @@
+
+const accountId = 1234
+let accountName = "hello"
+var accountEmail = "hello@google.com"
+accountpassward = "451255"
+let accountState;
+
+/*not prefare var
+becouse var can't differentiate between block of code
+*/
+
+accountName = "world!"
+
+ console.log(accountId);
+console.table({accountId, accountName, accountEmail, accountpassward, accountState})
